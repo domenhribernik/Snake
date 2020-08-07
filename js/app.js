36f6a65826321f03b7507e1e40b92334a0dcf58b
domenhribernik <domen.hribernik4@gmail.com>
2020-08-07 17:24:21 +0200

    adding the gameArr

--- js/app.js
+++ js/app.js
@@ -2,15 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
   let gameContainer = document.querySelector(".game")
   let numCols = 10;
   let numRows = 10;
+  let gameArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  
   for (let i = 0; i < numRows; i++) {
     for (let j = 0; j < numCols; j++) {
       let cell = document.createElement("div")
       cell.classList.add("game-cell")
       gameContainer.appendChild(cell)
-
-      // var btn = document.createElement("BUTTON");
-      // btn.innerHTML = "CLICK ME";
-      // document.body.appendChild(btn);   
+      //gameArr[i].push(j)
     }
   }
+  //console.log(gameArr);
+  let cellWidth = document.querySelector(".game-cell").getBoundingClientRect().width
+  gameContainer.style.width = (numCols * cellWidth) + "px" 
 })
