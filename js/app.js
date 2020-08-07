f1d86b2375f85480af583953f33088b2afc810c1
domenhribernik <domen.hribernik4@gmail.com>
2020-08-07 16:00:19 +0200

    proceduraly generating game grid

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
-let element = document.getElementById('h1')
-element.style.color = 'blue'
+document.addEventListener('DOMContentLoaded', () => {
+  let gameContainer = document.querySelector(".game")
+  let numCols = 10;
+  let numRows = 10;
+  for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
+      let cell = document.createElement("div")
+      cell.classList.add("game-cell")
+      gameContainer.appendChild(cell)
+
+      // var btn = document.createElement("BUTTON");
+      // btn.innerHTML = "CLICK ME";
+      // document.body.appendChild(btn);   
+    }
+  }
+})
