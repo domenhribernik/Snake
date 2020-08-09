c370c9f3291a8840565d9c1cfbefeb1d00fd9e9f
domenhribernik <domen.hribernik4@gmail.com>
2020-08-09 15:40:33 +0200

    createGrid function + removing node-sass addon

--- js/app.js
+++ js/app.js
@@ -4,18 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
   let numRows = 10;
   let gameArr = []
   
-  for (let i = 0; i < numRows; i++) {
-    gameArr.push([])
-    for (let j = 0; j < numCols; j++) {
-      let cell = document.createElement("div")
-      cell.classList.add("game-cell")
-      cell.classList.add("row" + i)
-      cell.classList.add("col" + j)
-      gameArr[i].push(cell)
-      gameContainer.appendChild(cell)
-      
+  function createGrid(Cols, Rows) {
+    for (let i = 0; i < Rows; i++) {
+      gameArr.push([])
+      for (let j = 0; j < Cols; j++) {
+        let cell = document.createElement("div")
+        cell.classList.add("game-cell")
+        cell.classList.add("row" + i)
+        cell.classList.add("col" + j)
+        gameArr[i].push(cell)
+        gameContainer.appendChild(cell)
+      }
     }
   }
+  createGrid(numCols, numRows)
+
   let cellWidth = document.querySelector(".game-cell").getBoundingClientRect().width
   gameContainer.style.width = (numCols * cellWidth) + "px" 
   gameArr[2][2].classList.add("snake")
